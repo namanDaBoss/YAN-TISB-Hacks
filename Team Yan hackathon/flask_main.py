ac430d379aa27aa5ff7d3a51efcf4a8f8aa2c0ae
samrath2007 <samrath1324@gmail.com>
2021-02-19 17:39:58 +0530

    FIxed bugs

--- Team Yan hackathon/flask_main.py
+++ Team Yan hackathon/flask_main.py
@@ -1,7 +1,9 @@
-from flask import Flask, redirect, render_template, request, session, url_for, flash
+from flask import Flask, redirect, render_template, request, session, url_for
 from flask_sqlalchemy import SQLAlchemy
 from flask_admin import Admin
 from flask_admin.contrib.sqla import ModelView
+import datetime
+
 
 app = Flask(__name__)
 app.secret_key = 'somesecretkeythatonlyishouldknow'
@@ -23,36 +25,46 @@ class User(db.Model):
 
 
 class Sport(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True, nullable=False)
     number_of_courts = db.Column(db.Integer)
     sport_name = db.Column(db.String(30), unique=True, nullable=False)
 
     def __repr__(self):
         return f'<Sport: {self.sport_name}>'
 
-class MyModelView(ModelView):
-    
-admin = Admin(app, name='Sports Booking', template_mode='bootstrap3')
-admin.add_view(ModelView(User, db.session))
-admin.add_view(ModelView(Sport, db.session))
 
 def is_admin():
-    if "username" in session:
-        if session["username"] == "000":
+    if 'username' in session:
+        if session['username'] == '000':
             return True
-        return False
+    return False
+
+
+class MyModelView(ModelView):
+    def is_accessible(self):
+        return is_admin()
+
+
+admin = Admin(app, name='Sports Booking', template_mode='bootstrap4')
+admin.add_view(MyModelView(User, db.session))
+admin.add_view(MyModelView(Sport, db.session))
+
+
 @app.route('/')
 @app.route('/home')
 def home():
-    if 'username' in session:
-        return render_template('index.html')
-    return redirect(url_for('login'))
+    if logged_in():
+        if is_admin():
+            return render_template('admin_index.html')
+        else:
+           return render_template('index.html', button_content="Booking", button_url=url_for('booking'))
+    return render_template('index.html', button_content="Login", button_url=url_for('login'))
 
 
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-    if 'username' not in session:
+    if not logged_in():
         if request.method == 'POST':
             session.pop('username', None)
             username = str(request.form['username'])
@@ -61,7 +73,7 @@ def login():
 
             if user_queried is not None:
                 if password == user_queried.password:
-                    session['username'] = user_queried.name
+                    session['username'] = user_queried.username
                     return redirect(url_for('home'))
         return render_template('login.html')
     return redirect(url_for('home'))
@@ -69,10 +81,32 @@ def login():
 
 @app.route("/logout")
 def logout():
-    if 'username' in session:
+    if logged_in():
         session.clear()
     return redirect(url_for('login'))
 
+def logged_in():
+    if 'username' in session:
+        return True
+    return False
 
+
+def today():
+    today = datetime.date.today()
+    return today
+
+
+def week_later():
+    today = datetime.date.today()
+    week_later = today + datetime.timedelta(days=7)
+    return week_later
+
+@app.route('/booking', methods=['GET', 'POST'])
+def booking():
+    if is_admin():
+        return redirect('/admin')
+    if logged_in():
+        return render_template('booking.html', min_date = today(), max_date = week_later())
+    return redirect(url_for('home'))
 if __name__ == '__main__':
     app.run(debug=True)
