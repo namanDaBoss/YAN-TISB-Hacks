9ceb8f276760ad90bcbe47e233def084836d6a7c
samrath2007 <samrath1324@gmail.com>
2021-02-19 23:16:44 +0530

    Summary(Required)

--- flask_main.py
+++ flask_main.py
@@ -59,103 +59,15 @@ admin.add_view(MyModelView(User, db.session))
 admin.add_view(MyModelView(Sport, db.session))
 
 
-def book(number_of_courts_available, booking):
-    cursor = conn.execute(
-        "select name from tisb where datetime = ? and sport=?;",
-        (booking.get("bookdatetime"), booking.get("sport")),
-    )
-    row = cursor.fetchall()
-    if len(row) < number_of_courts_available:
-        cursor = conn.execute(
-            "insert into tisb(name,datetime,sport)values \
-            (?,?,?)",
-            (booking.get("username"), booking.get(
-                "bookdatetime"), booking.get("sport")),
-        )
-        conn.commit()
-        return True
-    else:
-        return False
-
-
-def showRemainingCourts(booking):
-    number_of_courts_available = (
-        Sport.query.filter_by(
-            sport_name=booking.get("sport")).first().number_of_courts
-    )
-    cursor = conn.execute(
-        "select name from tisb where datetime = ?;", (booking.get(
-            "bookdatetime"))
-    )
-    row = cursor.fetchall()
-    remaining = number_of_courts_available - len(row)
-    return remaining
-
-
-def check(booking):
-    if len(booking.get("bookdatetime")) > 10:
-        timeOfBook = booking.get("bookdatetime")
-        cursor = conn.execute(
-            "select username from tisb where name =? and datetime like ? and sport=?;",
-            (booking.get("username"),
-             timeOfBook[:10] + "%", booking.get("sport")),
-        )
-    else:
-        cursor = conn.execute(
-            "select username from tisb where name =? and datetime =? and sport=?;",
-            (booking.get("username"), booking.get(
-                "bookdatetime"), booking.get("sport")),
-        )
-
-    row = cursor.fetchall()
-    if len(row) >= 2:
-        return False
-    else:
-        return True
-
-
 def str2datetime(string):
     datet = datetime.strptime(string, "%d-%m-%Y-%h")
     return datet
 
 
-def seeall(booking):
-    cursor = conn.execute(
-        "select name,datetime from tisb where sport = ?;", (
-            booking.get("sport"))
-    )
-
-    row = cursor.fetchall()
-
-    newli = []
-    for i in row:
-        a = list(i)
-        a[2] = str2datetime(a[2])
-        newli.append(a)
-
-    return newli
 
 
-def avail(booking):
-    number_of_courts_available = (
-        Sport.query.filter_by(
-            sport_name=booking.get("sport")).first().number_of_courts
-    )
-    cursor = conn.execute(
-        "select datetime from tisb where sport =?;", (booking.get("sport")))
-    row = cursor.fetchall()
-    times = []
-    for i in row:
-        bookingTime = booking.get("bookdatetime")
-        if i[0][:10] == bookingTime[:10]:
-            times.append(i[0][11:])
-    li = ["07", "08", "09", "10", "11", "12", "13",
-        "14", "15", "16", "17", "18", "19", "20", ]
-    availSlots = []
-    for i in li:
-        if times.count(i) < number_of_courts_available:
-            availSlots.append(i)
-    return availSlots
+
+
 
 
 @app.route("/")
@@ -226,12 +138,27 @@ def book_slot():
         datetime_to_func = appropiate_datetime_format(date, time)
 
         if Sport.query.filter_by(sport_name=sport_from_form).first() is not None:
-            book_thing = {
+            number_of_courts_available = 2
+            booking_thing = {
                 "username": "Samrath",
-                "bookdatetime": "21-02-2021-09",
+                "bookdatetime": "21-02-2021-10",
                 "sport": "Tennis"
             }
-            book(4,book_thing)
+            with sqlite3.connect("site.db") as conn:
+                cur = conn.cursor()
+                cursor = conn.execute(
+                    "select name from tisb where datetime = ? and sport=?;",
+                    (booking_thing.get("bookdatetime"), booking_thing.get("sport")),
+                )
+                row = cursor.fetchall()
+                if len(row) < number_of_courts_available:
+                    cursor = conn.execute(
+                        "insert into tisb(name,datetime,sport)values \
+                        (?,?,?)",
+                        (booking_thing.get("username"), booking_thing.get(
+                            "bookdatetime"), booking_thing.get("sport")),
+                    )
+                    conn.commit()
             return datetime_to_func + " " + sport_from_form
 
     if is_admin():
