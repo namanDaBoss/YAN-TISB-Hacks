35fd2df7494a6949aa444a2dca0b9e60a98f98d7
namanDaBoss <67775905+namanDaBoss@users.noreply.github.com>
2021-02-19 12:22:56 +0530

    Update database.py

--- database.py
+++ database.py
@@ -1,5 +1,5 @@
 import sqlite3
-import datetime
+from datetime import datetime
 
 conn = sqlite3.connect("tisb.db")
 cursor = conn.cursor()
@@ -10,43 +10,68 @@ cursor = conn.cursor()
   #  'email':'email2namanjain@gmail.com',
    # 'bookdatetime':'16-05-2003-09395',
     #'sport': 'badminton'
-    #}
+    #}
+#class booking():
+ #   now=datetime.now()
+  #  username='Arjun'
+   # email='agtherock13@gmail.com'
+    #bookdatetime=now.strftime("%d-%m-%Y")
+    #sport='badminton'
     
 
 
 def book(x,booking):
-    cursor = conn.execute("select name from tisb where datetime = '"+booking.get('bookdatetime')+"' and sport='"+booking.get('sport')+"'")
+    cursor = conn.execute("select name from tisb where datetime = '"+booking.bookdatetime+"' and sport='"+booking.sport+"'")
     row = cursor.fetchall()
     if len(row)<x:
         cursor=conn.execute("insert into tisb(name,email,datetime,sport)values \
-            (?,?,?,?)", (booking.get('username'),booking.get('email'),booking.get('bookdatetime'),booking.get('sport')))
+            (?,?,?,?)", (booking.username,booking.email,booking.bookdatetime,booking.sport))
         conn.commit()
-        print("y")
         return True
     else:
-        print("n")
         return False
 
 
 
 def showRemainingCourts(x,booking):
-    cursor = conn.execute("select name from tisb where datetime = '"+booking.get('bookdatetime')+"'")
+    cursor = conn.execute("select name from tisb where datetime = '"+booking.bookdatetime+"'")
     row = cursor.fetchall()
     remaining=x-len(row)
-    print(remaining)
     return remaining
 
 def check(booking):
-    if len(booking.get('bookdatetime'))>10:
+    if len(booking.bookdatetime)>10:
     
-        cursor=conn.execute("select email from tisb where name ='" + booking.get('username')+"' and datetime like '"+booking.get('bookdatetime')[:10]+"%' and sport='"+booking.get('sport')+"'")
+        cursor=conn.execute("select email from tisb where name ='" + booking.username+"' and datetime like '"+booking.bookdatetime[:10]+"%' and sport='"+booking.sport+"'")
         
     else:
         
-        cursor=conn.execute("select email from tisb where name ='" + booking.get('username')+"' and datetime ='"+booking.get('bookdatetime')+"' and sport='"+booking.get('sport')+"'")
+        cursor=conn.execute("select email from tisb where name ='" + booking.username+"' and datetime ='"+booking.bookdatetime+"' and sport='"+booking.sport+"'")
     row=cursor.fetchall()
     if len(row) >= 2:
         return False
     else:
         return True
+
+
+def str2datetime(string):
+    datet = datetime.strptime(string,"%d-%m-%Y")
+    return datet
+
+def seeall(booking):
+    cursor=conn.execute("select name,email,datetime from tisb where sport = '"+booking.sport+"'")
+
+    row=cursor.fetchall()
+
+    newli=[]
+    for i in row:
+        a=list(i)
+        sdate=a[2]
+        ddate=str2datetime(sdate)
+        a.remove(sdate)
+        a.insert(2,ddate)
+        newli.append(a)
+
+
+    return newli
 
