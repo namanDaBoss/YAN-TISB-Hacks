7025518fcd84d0010496964fa1fe746a49af4c6e
namanDaBoss <67775905+namanDaBoss@users.noreply.github.com>
2021-02-19 09:55:57 +0530

    fixed check function

--- database.py
+++ database.py
@@ -5,7 +5,13 @@ conn = sqlite3.connect("tisb.db")
 cursor = conn.cursor()
 
 #function for checking whether or not that slot is filled. If not, it inserts it.
-
+#booking= {
+ #   'username':'Naman',
+  #  'email':'email2namanjain@gmail.com',
+   # 'bookdatetime':'16-05-2003-09395',
+    #'sport': 'badminton'
+    #}
+    
 
 
 def book(x,booking):
@@ -31,9 +37,16 @@ def showRemainingCourts(x,booking):
     return remaining
 
 def check(booking):
-    cursor=conn.execute("select email from tisb where name ='" + booking.get('bookname')+"' and datetime ="+booking.datetime+"'")
+    if len(booking.get('bookdatetime'))>10:
+    
+        cursor=conn.execute("select email from tisb where name ='" + booking.get('username')+"' and datetime like '"+booking.get('bookdatetime')[:10]+"%' and sport='"+booking.get('sport')+"'")
+        
+    else:
+        
+        cursor=conn.execute("select email from tisb where name ='" + booking.get('username')+"' and datetime ='"+booking.get('bookdatetime')+"' and sport='"+booking.get('sport')+"'")
     row=cursor.fetchall()
-    if len(row) > 2:
+    if len(row) >= 2:
         return False
     else:
         return True
+
